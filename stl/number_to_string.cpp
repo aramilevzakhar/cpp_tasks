b12366ff312e67de326d50fc9d1c7611e82644b2
Zakhar <ar.z.v@yandex.ru>
2022-02-23 04:20:00 +0500

    I want to back ..

--- stl/number_to_string.cpp
+++ stl/number_to_string.cpp
@@ -7,31 +7,57 @@ class T {
 	private:
 		T* next;
 		int value;
+
+
 	public:
+
+
+		
+		T() {
+			this->next = NULL;
+		}
+		T(int p) {
+			this->next = NULL;
+			this->value = p;
+
+		}
+		
 		void addValue(int p) {
-			T *new_node = new T;
-			new_node->value = p;
-			new_node->next = NULL;
+			T *new_node = new T(p);
 
 			T* current_node = this;
+
 			while (current_node->next != NULL) {
 				current_node = current_node->next;
+
 			}
+
 			current_node->next = new_node;
+
+
 		}
 
 		void show_elements() {
 			T* current_node = this;
 			
 			while (current_node->next != NULL) {
-				std::cout << current_node->value << std::endl;
+				std::cout << "node value:\t" << current_node->value << "\nnode address:\t" << current_node << "\nnode next:\t" << current_node->next << "\n\n";
 				current_node = current_node->next;
 
 			}
-			std::cout << current_node->value << std::endl;
-
+			std::cout << "node value:\t" << current_node->value << "\nnode address:\t" << current_node << "\nnode next:\t" << current_node->next << "\n\n";
 
 		}
+		std::size_t length() {
+			T* current_node = this;
+			size_t length = 1;
+			while (current_node->next != NULL) {
+				current_node = current_node->next;
+				length++;
+			}
+			return length;
+		}
+
 };
 
 
@@ -50,13 +76,23 @@ int main() {
 	cout << three << endl;
 	cout << "My lst" << endl;
 	*/
-	T* t1 = new T;
+	T* t1 = new T(3);
+	T t2(1);
 
 	//t1->show();
 	t1->addValue(2);
 	t1->addValue(3);
 	t1->addValue(4);
 	t1->show_elements();
+	cout << t1->length() << "\n";
+	cout << endl;
+
+	t2.addValue(34);
+	t2.addValue(44);
+	t2.addValue(54);
+	t2.addValue(64);
+	t2.show_elements();
+	cout << t2.length() << "\n";
 	
 	return 0;
 }
