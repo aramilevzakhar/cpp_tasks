fa18ca033ecd2876cc9a80fb4b6cbb05dc1ffdf7
z9one <ar.z.v@yandex.ru>
2022-02-25 21:05:19 +0500

    add a iterator

--- aaa/main.cpp
+++ aaa/main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <unistd.h>
+#include <thread>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8080
+
+void print_1() {
+	using namespace std;
+	cout << "print 1" << endl;
+}
+
+//template <typename... Args, std::enable_if_t<
+void print_2() {
+	using namespace std;
+	cout << "print 2" << endl;
+}
+
+template<typename... Args, std::enable_if_t<sizeof...(Args) == 0, int> = 0>
+void print(const std::tuple<Args...>& t)
+{
+    std::cout << "()\n";
+}
+ 
+template<typename... Args, std::enable_if_t<sizeof...(Args) != 0, int> = 0>
+void print(const std::tuple<Args...>& t)
+{
+    std::cout << "(";
+    TuplePrinter<decltype(t), sizeof...(Args)>::print(t);
+    std::cout << ")\n";
+}
+
 
 
 
@@ -8,6 +42,7 @@ int main() {
 	using namespace std;
 	vector<int>a = {12, 321, 321};
 	list<int>b = {1, 2, 3};
+	int a1 = 0;
 
 	for (auto obj : a) {
 		cout << obj << endl;
@@ -18,17 +53,7 @@ int main() {
 		cout << obj << endl;
 	}
 
-
-
-
-
-
-
-
-
-
-
-
+	
 
 
 
