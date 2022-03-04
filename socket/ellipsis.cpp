086b5d95218378d00d1478d7322615e09dad4a72
z9one <ar.z.v@yandex.ru>
2022-03-04 16:54:39 +0500

    i\m sorry for my bad english

--- socket/ellipsis.cpp
+++ socket/ellipsis.cpp
@@ -5,21 +5,19 @@ double average(int count, ...) {
 	using namespace std;
 	va_list list;
 	va_start(list, count);
+
 	double avg = 0.0;
-	//for (int i = 0; i < 100; i++) {
-	cout << static_cast<double>(va_arg(list, int)) << endl;
-	cout << static_cast<double>(va_arg(list, int)) << endl;
-	cout << static_cast<double>(va_arg(list, int)) << endl;
-	cout << static_cast<double>(va_arg(list, int)) << endl;
-			//avg += static_cast<double>(va_arg(list, int)) / count;
-	//}
-	va_end(list);
+	for (int i = 0; i < count; i++) {
+		avg += static_cast<double>(va_arg(list, int)) / count;
+	}
+
 	return avg;
 }
 
 int main() {
 	using namespace std;
-	double avg = average(6, 1, 2, 3, 4, 5, 6);
+	// this is use to dangerous
+	double avg = average(100, 1, "Hello all around world", 3, 4, 5, 6);
 	cout << "Average is " << avg;
 	return 0;
 }
